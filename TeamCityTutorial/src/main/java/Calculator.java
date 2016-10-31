b4b9f9c4bb185cad58a340da776f088509b949ba
Bartek1817 <bartlomiejplonka@gmail.com>
2016-10-31 20:13:37 +0100

    Fixed Calculator problem

--- TeamCityTutorial/src/main/java/Calculator.java
+++ TeamCityTutorial/src/main/java/Calculator.java
@@ -1,6 +1,5 @@
 public class Calculator {
 	public static int add(int value1, int value2) {
-		if(value1 == 0) return 0;
 		return value1 + value2;
 	}
 	public static int substract(int minuend, int subtrahend) {
